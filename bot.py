d49fe583d1deeeb8793162b4b4fba5afe5c5a1f9
basilik52 <basilik52@gmail.com>
2020-05-21 14:03:11 +0700

    make it better

--- bot.py
+++ bot.py
@@ -16,51 +16,50 @@ amount = 0
 # dbuser='yrxxtoynomwkrz',
 # dbpassword='8164a0d936762b96651abde918d0c68c46739338a3f0cef7c8dd01214043b2b3',
 # namedb='df9nfputb06mls'
-
-@bot.message_handler(commands=["start"])
-def welcome(message):
-    with closing(psycopg2.connect(
+with closing(psycopg2.connect(
             host='ec2-34-198-243-120.compute-1.amazonaws.com',
             user='yrxxtoynomwkrz',
             password='8164a0d936762b96651abde918d0c68c46739338a3f0cef7c8dd01214043b2b3',
             dbname='df9nfputb06mls')) as connection:
-        with connection.cursor() as cursor:
-            sti = open('static/welcome.webp', 'rb')
-            bot.send_sticker(message.chat.id, sti)
+    @bot.message_handler(commands=["start"])
+    def welcome(message):
+            with connection.cursor() as cursor:
+                sti = open('static/welcome.webp', 'rb')
+                bot.send_sticker(message.chat.id, sti)
 
-            # keyboard
-            markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-            item1 = types.KeyboardButton("Удалить сумму")
-            item2 = types.KeyboardButton("Добавить сумму")
-            item3 = types.KeyboardButton("Реклама/отзыв")
-            item4 = types.KeyboardButton("Статистика трат")
-            item5 = types.KeyboardButton("О боте")
+                # keyboard
+                markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+                item1 = types.KeyboardButton("Удалить сумму")
+                item2 = types.KeyboardButton("Добавить сумму")
+                item3 = types.KeyboardButton("Реклама/отзыв")
+                item4 = types.KeyboardButton("Статистика трат")
+                item5 = types.KeyboardButton("О боте")
 
-            markup.add(item1, item2, item3, item4, item5)
-            bot.send_message(message.chat.id,
-                             "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный чтобы помочь тебе узнать свои траты за определенное время..".format(
-                                 message.from_user, bot.get_me()),
-                             parse_mode='html', reply_markup=markup)
+                markup.add(item1, item2, item3, item4, item5)
+                bot.send_message(message.chat.id,
+                                 "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный чтобы помочь тебе узнать свои траты за определенное время..".format(
+                                     message.from_user, bot.get_me()),
+                                 parse_mode='html', reply_markup=markup)
 
-            user_id = message.from_user.id
-            first_name = message.from_user.first_name
-            last_name = message.from_user.last_name
-            username = message.from_user.username
-            language_code = message.from_user.language_code
-            date_start = datetime.now()
-            print(user_id, first_name, last_name, username, language_code, date_start)
-            check_user = '''SELECT id_telegram FROM users WHERE id_telegram = %s'''
-            cursor.execute(check_user, [int(user_id)])
-            row = cursor.fetchone()
-            if row is None:
-                query = '''INSERT INTO users (id_telegram, first_name, last_name, username, language_code, created_at, updated_at) VALUES (%s,%s,%s,%s,%s,%s,%s)'''
-                cursor.execute(query, (
-                    int(user_id), str(first_name), str(last_name), str(username), str(language_code), str(date_start),
-                    str(date_start)))
-            else:
-                print("user - {} exist".format(user_id))
-        connection.commit()
-
+                user_id = message.from_user.id
+                first_name = message.from_user.first_name
+                last_name = message.from_user.last_name
+                username = message.from_user.username
+                language_code = message.from_user.language_code
+                date_start = datetime.now()
+                print(user_id, first_name, last_name, username, language_code, date_start)
+                check_user = '''SELECT id_telegram FROM users WHERE id_telegram = %s'''
+                cursor.execute(check_user, [int(user_id)])
+                row = cursor.fetchone()
+                if row is None:
+                    query = '''INSERT INTO users (id_telegram, first_name, last_name, username, language_code, created_at, updated_at) VALUES (%s,%s,%s,%s,%s,%s,%s)'''
+                    cursor.execute(query, (
+                        int(user_id), str(first_name), str(last_name), str(username), str(language_code), str(date_start),
+                        str(date_start)))
+                else:
+                    print("user - {} exist".format(user_id))
+            connection.commit()
+connection.close()
 
 @bot.message_handler(commands=["delete"])
 def delete(message):
