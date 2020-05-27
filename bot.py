e70c8f69eac6c5d8638e38a0dc7d54c433a56a59
basilik52 <basilik52@gmail.com>
2020-05-27 20:50:53 +0700

    make it better

--- bot.py
+++ bot.py
@@ -179,10 +179,10 @@ def get_buttons(message):
 
 def get_message(message):
     with closing(psycopg2.connect(
-            host='ec2-34-198-243-120.compute-1.amazonaws.com',
-            user='yrxxtoynomwkrz',
-            password='8164a0d936762b96651abde918d0c68c46739338a3f0cef7c8dd01214043b2b3',
-            dbname='df9nfputb06mls')) as connection:
+            host='ec2-34-202-88-122.compute-1.amazonaws.com',
+            user='psfxklbqjdysjc',
+            password='eaf1f4c9415008833090228825842986dcac8e6e269b1c9d430b7814a5f9ea97',
+            dbname='daro9jorij2fqh')) as connection:
         with connection.cursor() as cursor:
             id_telegram = message.from_user.id
             check_user = '''SELECT id FROM users WHERE id_telegram = %s'''
